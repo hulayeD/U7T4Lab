d64ead90798703a81da646a441f361c8e7979bcd
BT_1E10_28 <jeffreyc115@nycstudents.net>
2022-03-16 10:47:30 -0400

    Initial commit

---
+++ src/Runner.java
@@ -0,0 +1,48 @@
+import java.util.ArrayList;
+
+public class Runner
+{
+    public static void main(String[] args)
+    {
+        ArrayList<String> names = new ArrayList<String>();
+        names.add("Jessica");
+        names.add("Emily");
+        names.add("Destiny");
+        names.add("Mateo");
+        names.add("Sofia");
+        names.add("Tim");
+        names.add("James");
+
+//// before:
+//        System.out.println(names);
+//
+//// complete code below to traverse the arraylist and insert "HELLO" after any
+//// name that contains the letter "i"
+//        for (int i=0;i<names.size();i++)
+//        {
+//            String name=names.get(i);
+//            if(name.indexOf("i")!=-1)
+//            {
+//                names.add(i+1, "HELLO");
+//                i++;
+//            }
+//        }
+//
+//// after:
+//        System.out.println(names);
+        // before:
+        System.out.println(names2);
+
+// complete code below to traverse the arraylist and remove all
+// names that are exactly 5 letters long
+        for (
+        {
+
+        }
+
+// after:
+        System.out.println(names2);
+
+
+    }
+}
